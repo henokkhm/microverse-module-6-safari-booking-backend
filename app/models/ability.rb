607cd603368b2773bfc788b60b6b77f03caaa02f
katfogy <katfogy@gmail.com>
2024-01-19 11:03:05 +0100

    Cancancan gem file for role management installed and configured

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+   user ||= User.new
+    # can :read, Product, public: true
+    # can :read, Company, public: true
+    can :read, Safari, public: true
+    if user.persisted?
+      can :create, Reservation
+      can :read, Reservation, user_id: user.id
+    end
+    
+     can :manage, :all if user.role=="admin"
+  end
+end
