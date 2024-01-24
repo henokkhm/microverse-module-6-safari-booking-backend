8dae85619af9cf3fd104254560f84224d18664b8
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-24 15:58:36 +0000

    Add safari test

---
+++ spec/factories/safaris.rb
@@ -0,0 +1,14 @@
+# spec/factories/safaris.rb
+
+FactoryBot.define do
+    factory :safari do
+      name { 'Test Safari' }
+      location { 'Test Location' }
+      price { 100.0 }
+      description { 'A safari test' }
+      img { 'test_image.jpg' }
+  
+      # Other attributes as needed
+    end
+  end
+  
--- spec/safari_spec.rb
+++ spec/safari_spec.rb
@@ -1,20 +1,15 @@
 # spec/models/safari_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Safari, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:location) }
-
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than(0).is_less_than(1_000_000) }
-    it { should allow_value('100.50').for(:price) }
-    it { should_not allow_value('invalid_price').for(:price) }
-
     it { should validate_presence_of(:description) }
     it { should validate_length_of(:description).is_at_least(3) }
-
     it { should validate_presence_of(:img) }
   end
 
