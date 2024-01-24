c6b4f80b94ac5b63f1242331a0a2378805f63470
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2024-01-24 11:37:24 +0000

    delete deprecation

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -1,14 +1,8 @@
 Rswag::Ui.configure do |c|
-
-  # List the Swagger endpoints that you want to be documented through the
-  # swagger-ui. The first parameter is the path (absolute or relative to the UI
-  # host) to the corresponding endpoint and the second is a title that will be
-  # displayed in the document selector.
-  # NOTE: If you're using rspec-api to expose Swagger files
-  # (under openapi_root) as JSON or YAML endpoints, then the list below should
-  # correspond to the relative paths for those endpoints.
-
-  c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+  # Use openapi_endpoint instead of swagger_endpoint
+  c.openapi_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs' do |config|
+    # You can configure additional options here if needed
+  end
 
   # Add Basic Auth in case your API is private
   # c.basic_auth_enabled = true
