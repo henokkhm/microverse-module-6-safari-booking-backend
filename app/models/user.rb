6410a4992961b61e6a4711f0e476276dfe7e521b
katfogy <katfogy@gmail.com>
2024-01-29 16:50:07 +0100

    Rubocop Error Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :reservations, dependent: :destroy
   after_initialize :set_default_role, if: :new_record?
+  
   def set_default_role
     self.role ||= :user
   end
