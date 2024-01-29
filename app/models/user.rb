4145ede64c12c716bb67f1c634cf5df8d89eafa2
katfogy <katfogy@gmail.com>
2024-01-29 16:47:07 +0100

    Rubocop Error Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,8 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
-
   has_many :reservations, dependent: :destroy
-
   after_initialize :set_default_role, if: :new_record?
-
-
   def set_default_role
     self.role ||= :user
   end
